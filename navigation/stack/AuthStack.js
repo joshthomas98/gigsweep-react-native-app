d751141e5cb313745996d281d02c570cbe3602b5
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2024-01-28 03:04:32 +0000

    Login functionality coompleted

--- navigation/stack/AuthStack.js
+++ navigation/stack/AuthStack.js
@@ -9,10 +9,7 @@ const Stack = createStackNavigator();
 
 const AuthStack = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="Welcome"
-    >
+    <Stack.Navigator initialRouteName="Welcome">
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Main" component={MainStack} />
